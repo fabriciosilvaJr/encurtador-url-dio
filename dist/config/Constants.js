96221c563d491848ac960ec193a06d212f30b34d
fabriciosilvaJr <fabriciosilva187@gmail.com>
2022-06-12 12:35:08 -0300

    Conexão com Mongo

--- dist/config/Constants.js
+++ dist/config/Constants.js
@@ -2,6 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.config = void 0;
 exports.config = {
-    API_URL: "http://localhost:5000"
+    API_URL: "http://localhost:5000",
+    MONGO_CONNECTION: "mongodb+srv://fos:Mo3xBaauDGf0ELzK@cluster0.1zfqr.mongodb.net/?retryWrites=true&w=majority"
 };
 //# sourceMappingURL=Constants.js.map
--- src/config/Constants.ts
+++ src/config/Constants.ts
@@ -1,3 +1,5 @@
 export const config ={
-    API_URL: "http://localhost:5000"
+    API_URL: "http://localhost:5000",
+    MONGO_CONNECTION: 
+    "mongodb+srv://fos:Mo3xBaauDGf0ELzK@cluster0.1zfqr.mongodb.net/?retryWrites=true&w=majority"
 }
