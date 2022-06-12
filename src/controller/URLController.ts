13e32a18651d6068cada3ad3e4e2620206a3317f
fabriciosilvaJr <fabriciosilva187@gmail.com>
2022-06-12 13:56:45 -0300

    commit

--- src/controller/URLController.ts
+++ src/controller/URLController.ts
@@ -1,3 +1,4 @@
+import { URLModel } from '../database/URL';
 import {Request, Response} from 'express';
 import shortId from 'shortid';
 import {config} from '../config/Constants'
@@ -5,28 +6,37 @@ import {config} from '../config/Constants'
 export class URLController{
     public async shorten(req: Request, response:Response): Promise<void>{
         // Verificar se a url existe
-        // Criar o hash para url
         const {originURL} = req.body;
+        const url = await URLModel.findOne({originURL});
+        if(url){
+            response.json(url);
+            return
+        }
         const hash = shortId.generate();
         const shortURL =  `${config.API_URL}/${hash}`
 
         // Salvar a Url no banco
+        const newUrl = await URLModel.create({hash, shortURL, originURL})
         // Retornar a Url que salvamos
-        response.json({originURL, hash, shortURL})
+        response.json({newUrl})
    
     }
 
     public async redirect(req: Request, response:Response): Promise<void>{
-        // Pegar hash na URL
         const {hash} = req.params
-        // Encontrar a URL orginal pelo hash
-        const url = {
-            originURL: "https://cloud.mongodb.com/v2/62a3ebb85f16a24e469b4357#clusters",
-            hash: "6FqOh_oiw",
-            shortURl:"http://localhost:5000/6FqOh_oiw"
-        }
-        // Redirecionar para a URL original a partir do que encontramos no DB
+       const url = await URLModel.findOne({hash});
+
+        if(url){
+
             response.redirect(url.originURL)
+            return
+        }
+
+        response.status(400).json({ error: 'URL not found' })
+
+      
+
+ 
     }
 
 }
